b00995eb2aeca1ef989103712cfa813ed4acc50c
小帅 <279589564@qq.com>
2022-08-23 00:31:16 +0800

    Initial Commit

---
+++ src/main/java/com/xs/SpringTestGitApplication.java
@@ -0,0 +1,13 @@
+package com.xs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestGitApplication.class, args);
+    }
+
+}
